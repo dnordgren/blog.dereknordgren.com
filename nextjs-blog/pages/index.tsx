fb05c2a1b5b9d13edaedd64709aae818e14b4e5b
Derek Nordgren <derek.nordgren@hudl.com>
2021-05-03 11:42:37 +0100

    Add some real content - profile links, about me.

--- nextjs-blog/pages/index.tsx
+++ nextjs-blog/pages/index.tsx
@@ -1,7 +1,7 @@
-import Head from 'next/head'
-import Link from 'next/link'
-import Layout, { siteTitle } from '../components/layout'
-import utilStyles from '../styles/utils.module.css'
+import Head from 'next/head';
+import Link from 'next/link';
+import Layout, { siteTitle } from '../components/layout';
+import utilStyles from '../styles/utils.module.css';
 
 export default function Home() {
   return (
@@ -10,14 +10,51 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Personal Introduction]</p>
         <p>
-          Check out my first post - {' '}
-          <Link href="/posts/first-post">
+          Engineering Manager at{' '}
+          <a href="www.hudl.com" target="_blank" rel="noopener noreferrer">
+            Hudl
+          </a>
+          .
+        </p>
+      </section>
+      <p>
+        I lead the Hudl team that builds and maintains the AWS cloud
+        infrastructure that powers the world of sport with video and analytics.
+      </p>
+      <section className={utilStyles.headingMd}>
+        <p>
+          More about me{' '}
+          <Link href="/posts/about-me">
             <a>here</a>
           </Link>
+          . Find me on{' '}
+          <a
+            href="github.com/dnordgren/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            GitHub
+          </a>
+          ,{' '}
+          <a
+            href="www.linkedin.com/in/dereknordgren/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            LinkedIn
+          </a>
+          ,{' '}
+          <a
+            href="twitter.com/dereknordgren/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Twitter
+          </a>
+          .
         </p>
       </section>
     </Layout>
-  )
+  );
 }
